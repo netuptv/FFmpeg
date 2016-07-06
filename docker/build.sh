9612a69978cf1c8a8fc77ecde5988853e606f756
Vladimir Kaunkin <kvi@netup.ru>
2016-07-06 17:37:04 +0300

    added docker build scripts

---
+++ docker/build.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -ex
+
+TARGET=ffmpeg
+SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
+SRC_DIR=${SCRIPT_DIR}/..
+#BUILD_DIR=$(pwd)/build/${TARGET}
+REVISION=$(cd ${SRC_DIR}; git rev-parse HEAD)
+OUT_DIR=$(pwd)/out/${TARGET}
+CCACHE_DIR=$(pwd)/ccache/${TARGET}
+IMAGE_TAG=$(head -n 1 ${SCRIPT_DIR}/build.tag)
+IMAGE_NAME=build.netup:5000/iptv_2.0_build:${IMAGE_TAG}
+
+mkdir -p ${BUILD_DIR} ${OUT_DIR} ${CCACHE_DIR}
+
+#    --volume ${BUILD_DIR}:/mnt/build \
+docker run --rm \
+    --volume ${SRC_DIR}:/mnt/src \
+    --volume ${OUT_DIR}:/mnt/out \
+    --volume ${CCACHE_DIR}:/mnt/ccache \
+    --user ${UID} \
+    -t \
+    ${IMAGE_NAME} \
+    /mnt/src/docker/scripts/make.sh $@
+
+printf "%s_revision=%s\n" "${TARGET}" "${REVISION}" > ${OUT_DIR}/build.info
---
+++ docker/scripts/make.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SRC_DIR=$(cd ${SCRIPT_DIR}/../..; pwd)
+OUT_DIR=/mnt/out/
+CCACHE_DIR=/mnt/ccache
+
+cd ${SRC_DIR}
+
+export PATH=/usr/lib/ccache/:${PATH}
+export CCACHE_DIR
+
+./configure --prefix=${OUT_DIR}
+make -j $(nproc)
+make install
