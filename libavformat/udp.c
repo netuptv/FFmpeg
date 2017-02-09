e70aabd7b28b8faa1e4331943a89a0e0c46438fe
Constantine Yemelyanov <kostya.emelyanov@gmail.com>
2017-02-09 15:44:05 +0300

    Changed lower level of muxrate from 100 to 0, otherwise it was not possible to set muxrate == 0. Ref T1229.

--- libavformat/udp.c
+++ libavformat/udp.c
@@ -138,7 +138,7 @@ static const AVOption options[] = {
     { "localaddr",      "Local address",                                   OFFSET(localaddr),      AV_OPT_TYPE_STRING, { .str = NULL },               .flags = D|E },
     { "udplite_coverage", "choose UDPLite head size which should be validated by checksum", OFFSET(udplite_coverage), AV_OPT_TYPE_INT, {.i64 = 0}, 0, INT_MAX, D|E },
     { "pkt_size",       "Maximum UDP packet size",                         OFFSET(pkt_size),       AV_OPT_TYPE_INT,    { .i64 = 1472 },  -1, INT_MAX, .flags = D|E },
-    { "muxrate",        "Stream muxrate in bits per second. VBR if unset.",OFFSET(muxrate),        AV_OPT_TYPE_INT,    { .i64 = 0 },    100,  100000, E },
+    { "muxrate",        "Stream muxrate in bits per second. VBR if unset.",OFFSET(muxrate),        AV_OPT_TYPE_INT,    { .i64 = 0 },      0,   20000, E },
     { "reuse",          "explicitly allow reusing UDP sockets",            OFFSET(reuse_socket),   AV_OPT_TYPE_INT,    { .i64 = -1 },    -1, 1,       D|E },
     { "reuse_socket",   "explicitly allow reusing UDP sockets",            OFFSET(reuse_socket),   AV_OPT_TYPE_INT,    { .i64 = -1 },    -1, 1,       .flags = D|E },
     { "broadcast", "explicitly allow or disallow broadcast destination",   OFFSET(is_broadcast),   AV_OPT_TYPE_INT,    { .i64 = 0  },     0, 1,       E },
