93994ee63de618631da98157c6b6af1c90bcb141
kvi <kvi@netup.ru>
2017-09-01 14:20:44 +0300

    add --shared option

--- docker/scripts/make.sh
+++ docker/scripts/make.sh
@@ -13,5 +13,22 @@ cd ${BUILD_DIR}
 export PATH=/usr/lib/ccache/:${PATH}
 export CCACHE_DIR
 
-${SRC_DIR}/configure --prefix=${OUT_DIR} --enable-pic --enable-libmp3lame --disable-vaapi
+OPTS=$( getopt -o '' --long "shared" -n "$( basename ${0} )" -- "$@" )
+eval set -- "${OPTS}"
+
+SHARED=
+
+while true; do
+    case "${1}" in
+        --shared)
+            SHARED=--enable-shared
+            ;;
+        --)
+            shift
+            break
+    esac
+    shift
+done
+
+${SRC_DIR}/configure --prefix=${OUT_DIR} --enable-pic --enable-libmp3lame --disable-vaapi ${SHARED}
 make -j $(nproc) install-libs install-headers
