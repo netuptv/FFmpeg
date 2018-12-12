da5347f442c0aef660c48afdb95ef126d53d086d
kvi <kvi@netup.ru>
2018-12-12 19:16:18 +0300

    fix(build): add build with ssl1.1; fixes T3681
    auditors: alex3d

---
+++ docker/build_ssl1.1.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+set -e
+
+SCRIPT_DIR="$(realpath "$(dirname "${0}")")"
+SRC_DIR="${SCRIPT_DIR}/.."
+OUT_DIR="$(pwd)/out/ffmpeg"
+BUILD_DIR="$(pwd)/build/ffmpeg"
+CCACHE_DIR="$(pwd)/ccache/ffmpeg"
+BUILD_PACKAGE_DIR="${BUILD_DIR}/package"
+REVISION="$(cd ${SRC_DIR}; git rev-parse HEAD)"
+[ -z "${BRANCH_NAME}" ] && \
+    BRANCH_NAME=$(cd ${SRC_DIR}; git symbolic-ref -q --short HEAD || echo 'unknown')
+if [ -z "${BUILD_NUMBER}" ]; then
+    BUILD_NUMBER="$( date --utc '+%Y%m%dT%H%M' )-$( id -un )"
+fi
+TAG="$( sed 's#/#_#g' <<<"${BRANCH_NAME}" ).${BUILD_NUMBER}"
+
+mkdir -p "${OUT_DIR}" "${CCACHE_DIR}" "${BUILD_DIR}"
+
+. "${SCRIPT_DIR}/image/image.env"
+SNAPSHOT_URL="http://snapshot.debian.org/archive/debian/${SNAPSHOT}"
+docker build --force-rm --iidfile "${BUILD_DIR}/image.id" - <<EOF
+FROM debian:${IMAGE_TAG}
+
+RUN echo 'deb ${SNAPSHOT_URL} stretch main' > /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get install --no-install-recommends --assume-yes \
+        gcc make libssl-dev libc6-dev yasm libmp3lame-dev pkg-config && \
+    apt-get install --no-install-recommends --assume-yes ccache && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+EOF
+IMAGE_ID="$( head -n1 "${BUILD_DIR}/image.id" )"
+
+docker run --rm \
+    --volume ${SRC_DIR}:/mnt/src:ro \
+    --volume ${OUT_DIR}:/opt/ffmpeg \
+    --volume ${BUILD_DIR}:/mnt/build \
+    --volume ${CCACHE_DIR}:/mnt/ccache \
+    --user ${UID} \
+    --tty \
+    ${IMAGE_ID} \
+    /mnt/src/docker/scripts/make.sh $@
+
+printf '%s_revision="%s %s %s"\n' "${TARGET}" "${REVISION}" "${BRANCH_NAME}" "${BUILD_URL}" > ${OUT_DIR}/build.info
